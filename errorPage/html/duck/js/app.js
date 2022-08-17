18a4cc8c9f04a3d0ea898b9c8d18b4503e9ff519
mpdcampbell <47434940+mpdcampbell@users.noreply.github.com>
2022-08-17 23:13:28 +0100

    Update swimming animation script

--- errorPage/html/duck/js/app.js
+++ errorPage/html/duck/js/app.js
@@ -1,11 +1,11 @@
-tsParticles.load("tsparticles", {
+tsParticles.load("leftDucks", {
       particles: {
     number: {
-      value: 350,
+      value: 10,
       limit:0,
       density: {
-        enable: false,
-        value_area: 600
+	enable: true,
+	value_area: 400
       }
     },
     color: {
@@ -17,118 +17,87 @@ tsParticles.load("tsparticles", {
         width: 2,
         color: "#fff"
       },
-      polygon: {
-        nb_sides: 6
-      },
       image: [{
-        src: "images/bee_sx.png",
-        width: 64,
-        height: 64
-      },
-      {
-        src: "images/bee_dx.png",
+	src: "https://error.codeslikeaduck.com/images/leftCodeDuck.png",
         width: 64,
         height: 64
       }],
-      images: [{
-        src: "images/bee_sx.png",
-        width: 64,
-        height: 64
-      },
-      {
-        src: "images/bee_dx.png",
-        width: 64,
-        height: 64
-      }]
     },
     opacity: {
       value: 1,
-      random: false,
-      anim: {
-        enable: false,
-        speed: 2,
-        opacity_min: 0.2,
-        sync: false
-      }
     },
     size: {
       value: 16,
-      random: false,
-      anim: {
-        enable: false,
-        speed: 20,
-        size_min: 32,
-        sync: false
-      }
-    },
-    line_linked: {
-      enable: false,
-      distance: 50,
-      color: "#ffffff",
-      opacity: 0.5,
-      width: 1
     },
     move: {
       enable: true,
       speed: 5,
-      direction: "none",
+      direction: "left",
       random: true,
       straight: false,
+      collisions: false,
       outModes: 
       {
         default:"bounce",
         top:"bounce",
-        left:"bounce",
-        right:"bounce",
+        left:"out",
+        right:"out",
         bottom:"bounce"
       },
-      bounce: false,
-      attract: {
-        enable: true,
-        rotateX: 250,
-        rotateY: 1000
-      },
-
+      bounce: true,
     },
   },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: {
-        enable: true,
-        mode:  "bubble"
-      },
-      onclick: {
-        enable: true,
-        mode: "repulse"
-      },
-      resize: true
-    },
-    modes: {
-      grab: {
-        distance: 125,
-        line_linked: {
-          opacity: 1
-        }
-      },
-      bubble: {
-        distance: 200,
-        size: 33,
-        duration: 2,
-        opacity: 6,
-        speed: 2
-      },
-      repulse: {
-        distance: 200,
-        duration: 0.25
-      },
-      push: {
-        particles_nb: 4
-      },
-      remove: {
-        particles_nb: 2
+  retina_detect: true
+});
+
+tsParticles.load("rightDucks", {
+      particles: {
+    number: {
+    value: 10,
+    limit:0,
+    density: {
+      enable: true,
+      value_area: 400
       }
-    }
+    },
+    color: {
+      value: "#ffffff"
+    },
+    shape: {
+      type: "image",
+      stroke: {
+        width: 2,
+        color: "#fff"
+      },
+      image: [{
+	src: "https://error.codeslikeaduck.com/images/rightCodeDuck.png",
+        width: 64,
+        height: 64
+      }],
+    },
+    opacity: {
+      value: 1,
+    },
+    size: {
+      value: 16,
+    },
+    move: {
+      enable: true,
+      speed: 5,
+      direction: "right",
+      random: true,
+      straight: false,
+      collisions: false,
+      outModes: 
+      {
+        default:"bounce",
+        top:"bounce",
+        left:"out",
+        right:"out",
+        bottom:"bounce"
+      },
+      bounce: true,
+    },
   },
   retina_detect: true
 });
